d6e837f2dbf9fcdc151d78f17cc35b0f80e54abd
haifeng.jiang <haifeng.jiang@JiangdeMacBook-Pro.local>
2024-01-26 14:52:50 +0800

    another way for 1986

--- 19/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cc
+++ 19/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cc
@@ -10,6 +10,40 @@ using namespace std;
 class Solution {
 public:
 
+
+    int minSessions(vector<int>& tasks, int sessionTime) {
+        int n = tasks.size();
+        int m = (1 << n);
+        vector<int> oneSessions;
+        vector<int> dp(m, -1);
+        dp[0] = 0;
+        for (int i=1; i<m; i++) {
+            int sum = 0;
+            for (int j=0; j<n; j++) {
+                if (((1<<j)&i)) sum += tasks[j];
+                while (sum > sessionTime) break;
+            }
+            if (sum <= sessionTime) oneSessions.push_back(i);
+        }
+        return dfs(dp, oneSessions, m-1);
+    }
+
+
+    int dfs(vector<int>& dp, vector<int>& v, int k) {
+        if (dp[k] >= 0) return dp[k];
+        dp[k] = 15;
+        for (auto i : v) {
+            if ((i&k) == i) {
+                dp[k] = min(dp[k], dfs(dp, v, (k&(~i)))+1);
+            }
+        }
+        return dp[k];
+    }
+};
+
+class Solution1 {
+public:
+
     int sessionTime;
     vector<int> tasks;
     
--- a.cc
+++ a.cc
@@ -10,65 +10,34 @@ using namespace std;
 class Solution {
 public:
 
-    int sessionTime;
-    vector<int> tasks;
-    
-    using LL = long long;
 
     int minSessions(vector<int>& tasks, int sessionTime) {
-        this->sessionTime = sessionTime;
-        this->tasks = tasks;
-        int lo=1, hi=tasks.size()+1;
-        while (lo < hi) {
-            int m = (lo + hi) / 2;
-            if (check(m)) hi = m;
-            else lo = m+1;
+        int n = tasks.size();
+        int m = (1 << n);
+        vector<int> oneSessions;
+        vector<int> dp(m, -1);
+        dp[0] = 0;
+        for (int i=1; i<m; i++) {
+            int sum = 0;
+            for (int j=0; j<n; j++) {
+                if (((1<<j)&i)) sum += tasks[j];
+                while (sum > sessionTime) break;
+            }
+            if (sum <= sessionTime) oneSessions.push_back(i);
         }
-        return lo;
+        return dfs(dp, oneSessions, m-1);
     }
 
-    LL encode(int i, map<int,int>& m) {
-        LL ans = i;
-        for (auto [k, c] : m) {
-            for (int i=0; i<c; i++) ans = (ans << 4) | k;
-        }
-        return ans;
-    }
 
-    pair<int, map<int,int>> decode(LL x, int n) {
-        map<int,int> m;
-        for (int i=0; i<n; i++) {
-            m[x&15]++;
-            x = x >> 4;
-        }
-        return {x, m};
-    }
-
-    bool helper(LL cur, int n, unordered_map<LL, bool>& um) {
-        if (um.count(cur)) return um[cur];
-        auto [index, m] = decode(cur, n);
-        if (index == tasks.size()) return true;
-        vector<int> tmp;
-        for (auto [k, _] : m) {
-            if (k+tasks[index] <= sessionTime) tmp.push_back(k);
-        }
-        for (auto k : tmp) {
-            if (k+tasks[index] <= sessionTime) {
-                m[k]--;
-                m[k+tasks[index]]++;
-                LL next = encode(index+1,m);
-                if (helper(next, n, um)) return um[cur] = true;
-                m[k]++;
-                m[k+tasks[index]]--;
+    int dfs(vector<int>& dp, vector<int>& v, int k) {
+        if (dp[k] >= 0) return dp[k];
+        dp[k] = 15;
+        for (auto i : v) {
+            if ((i&k) == i) {
+                dp[k] = min(dp[k], dfs(dp, v, (k&(~i)))+1);
             }
         }
-        return um[cur] = false;
-    }
-
-    bool check(int n) {
-        if (n >= tasks.size()) return true;
-        unordered_map<LL, bool> m;
-        return helper(0, n, m);
+        return dp[k];
     }
 
 };
